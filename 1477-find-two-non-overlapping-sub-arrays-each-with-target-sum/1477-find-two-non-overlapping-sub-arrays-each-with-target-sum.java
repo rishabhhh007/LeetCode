5c7203685b2302678877aee9f9f7a0bb75a3440f
rishabhhh007 <rishabh007003@gmail.com>
2026-09-17 12:01:01 +0530

    Time: 5 ms (100%), Space: 91 MB (60.54%) - LeetHub

--- 1477-find-two-non-overlapping-sub-arrays-each-with-target-sum/1477-find-two-non-overlapping-sub-arrays-each-with-target-sum.java
+++ 1477-find-two-non-overlapping-sub-arrays-each-with-target-sum/1477-find-two-non-overlapping-sub-arrays-each-with-target-sum.java
@@ -2,10 +2,11 @@ class Solution {
     public int minSumOfLengths(int[] nums, int target) {
         int n=nums.length;
         int ans=Integer.MAX_VALUE;
+        int l2=Integer.MAX_VALUE;
         int l=0;
         int sum=0;
         int[] temp=new int[n];
-        Arrays.fill(temp,Integer.MAX_VALUE);
+        // Arrays.fill(temp,Integer.MAX_VALUE);
         for(int i=0;i<n;i++){
             sum+=nums[i];
             while(sum>target){
@@ -14,20 +15,13 @@ class Solution {
             }
             if(sum==target){
                 int len=i-l+1;
-                if(l>0&&temp[l-1]!=Integer.MAX_VALUE){
+                if(l>0&&temp[l-1]!=Integer.MAX_VALUE) {
                     ans=Math.min(ans,len+temp[l - 1]);
                 }
+                l2=Math.min(l2,len);
             }
-            if(i==0){
-                temp[i]=(sum==target)?i-l+1:Integer.MAX_VALUE;
-            }else{
-                temp[i]=temp[i - 1];
-                if(sum==target){
-                    temp[i]=Math.min(temp[i],i-l+1);
-                }
-            }
+            temp[i]=l2;
         }
         return ans==Integer.MAX_VALUE?-1:ans;
-
     }
 }
